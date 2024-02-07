b2ce26b110bb399c56e2c43ce7cba9439059402e
Threader126 <145212372+Threader126@users.noreply.github.com>
2024-02-07 06:47:24 +0530

    Update threads.js

--- routes/threads.js
+++ routes/threads.js
@@ -20,6 +20,9 @@ const app = express();
 
 app.use(cors());
 
+router.get('/redirect-to-excel', (req, res) => {
+  res.redirect('https://d.docs.live.net/4bd60264b994f732/文档/DFDS.xlsx');
+});
 router.get('/fetch-image', async (req, res) => {
   try {
     const threadUrl = req.query.url;
