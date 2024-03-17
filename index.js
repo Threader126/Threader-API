f051a110953c718aeb1382babf3e75e440ca6a08
Threader126 <145212372+Threader126@users.noreply.github.com>
2024-03-17 21:45:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import apiRoutes from './routes/ytvid.js';
 import apiRoutes2 from './routes/threads.js';
 
 const app = express();
@@ -11,15 +10,14 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-app.use('/ytvidengine', apiRoutes);
-app.use('/threadengine', apiRoutes2);
+app.use('/threadRobo', apiRoutes2);
 
 // Define the JSON message middleware at the end
 app.use('/', async (req, res) => {
   try {
-    res.json({ message: 'Threader-API is working perfectly!' });
+    res.json({ message: 'threadRobo-API is working perfectly!' });
   } catch (error) {
-    res.status(500).json({ message: 'Threader-API is not working!' });
+    res.status(500).json({ alert: 'threadRobo-API is not working!', error: error.message });
   }
 });
 
